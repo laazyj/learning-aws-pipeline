5dd0c98d11fc364c8d48f26f126b513304dbd131
Jason Duffett <jasonduffett@Jasons-MBP.lan>
2022-08-21 15:34:09 +0100

    Add build stage

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
+import { SecretValue } from 'aws-cdk-lib';
+import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new Pipeline(this, 'Pipeline', {
+      pipelineName: 'Pipeline',
+      crossAccountKeys: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const sourceOutput = new Artifact('SourceOutput');
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new GitHubSourceAction({
+          actionName: 'Pipeline_Source',
+          owner: 'laazyJ',
+          repo: 'learning-aws-pipeline',
+          branch: 'master',
+          oauthToken: SecretValue.secretsManager('github-token'),
+          output: sourceOutput,
+        })
+      ],
+    });
+
+    const cdkBuildOutput = new Artifact('CdkBuildOutput');
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new CodeBuildAction({
+          actionName: 'CdkBuild',
+          project: new PipelineProject(this, 'CdkBuildProject', {
+            environment: {
+              buildImage: LinuxBuildImage.STANDARD_6_0
+            },
+            buildSpec: BuildSpec.fromSourceFilename('build-specs/cdk-build-spec.yml'),
+          }),
+          input: sourceOutput,
+          outputs: [cdkBuildOutput],
+        })
+      ]
+    });
   }
 }
