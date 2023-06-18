2678ba63f56bf3c03764ccd0185a5150a5d3bc28
Jason Duffett <git@jasonduffett.org>
2023-06-18 11:29:49 +0100

    test: add snapshot tests

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -2,11 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as Pipeline from '../lib/pipeline-stack';
 
-test('Empty Stack', () => {
-   const app = new cdk.App();
-     // WHEN
-   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-     // THEN
-   const template = Template.fromStack(stack);
-   template.templateMatches({ /* empty */ });
-});
+describe("PipelineStack", () => {
+  test("to match snapshot", () => {
+    const app = new cdk.App();
+      // WHEN
+    const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
+      // THEN
+    const template = Template.fromStack(stack);
+    console.info(template);
+    expect(template).toMatchSnapshot();
+  });
+}); 
